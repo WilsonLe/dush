df2fd336b352e0bb81a5dfb5d089fd1ed648047c
wilsonle <leanhminh2907@gmail.com>
2022-09-14 18:37:42 -0400

    add necessary global vars

--- dush.c
+++ dush.c
@@ -6,6 +6,10 @@ int main()
 {
 	char *inputString;
 	char *exitString = "exit";
+	const int MAX_PATH = 256;
+	const int MAX_PATH_CHAR = 256;
+	char path[MAX_PATH][MAX_PATH_CHAR];
+
 	size_t bufsize = __INT_MAX__;
 	size_t characters;
 
@@ -23,12 +27,21 @@ int main()
 		printf("> ");
 		getline(&inputString, &bufsize, stdin);
 
+		// start edit from here
+
+		// handleBuiltInCommands(inputString, path)
 		if (strcmp(inputString, exitString) != 0)
 		{
 			// call exit system call
 			printf("Exited");
 			break;
 		}
+
+		// any command that made it here is not a built in command
+
+		// string programPath = pathParser()
+
+		// exec on the program path
 
 		// data handling
 		printf("You typed: %s\n", inputString);
